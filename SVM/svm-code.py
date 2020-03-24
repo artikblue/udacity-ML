16526838681f6bc8ed95b1ce0652ca9e29f800ff
artikblue <artikblue@protonmail.com>
2020-03-24 20:35:33 +0100

    svm a bit more explained

--- SVM/svm-code.py
+++ SVM/svm-code.py
@@ -1,13 +1,16 @@
 import sys
 from class_vis import prettyPicture
 from prep_terrain_data import makeTerrainData
+import matplotlib
+matplotlib.use('agg')
 
+import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import copy
 import numpy as np
 import pylab as pl
 
-
+from class_vis import prettyPicture, output_image
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
@@ -15,6 +18,14 @@ features_train, labels_train, features_test, labels_test = makeTerrainData()
 ### we handle the import statement and SVC creation for you here
 from sklearn.svm import SVC
 clf = SVC(kernel="rbf") # kernel='linear', poly, 
+# linear = straight line
+# rbf = more custom and complex form
+# sigmoid = a sigmoid (S like function)
+# C parameter =C controls the cost of misclassification on the training data.
+# large C = more points correct and more overfitting ()
+# gamma parameter = Intuitively, the gamma parameter defines how far the influence of a single training example reaches, with low values meaning ‘far’ and high values meaning ‘close’.
+# large gamma = low variance and high bias
+# large C = high variance and low bias
 clf.fit(features_train, labels_train)
 
 
@@ -41,4 +52,3 @@ In multilabel classification, this function computes subset accuracy: the set of
 acc = accuracy_score(pred, labels_test)
 
 print(acc)
-
